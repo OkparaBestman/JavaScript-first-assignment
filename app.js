05232811484564217c73920f816427f9428d3351
bezites <bezites18@gmail.com>
2023-09-21 13:07:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+function prod(kg,b){
+let kgTopounds = kg*b;
+    return kgTopounds
+    
+};
+console.log(prod(80,2.205));
+let Name = String(prompt("Kindly input your name"));
+let Wieght = Number(prompt("Kindly input your weight(kg)"));
+   
+const mydetials = (Name, Weight) => {
+    return `My name is ${Name} my weight is ${Weight}.`
+}
+console.log(mydetials("Bestman","80kg" ));
+
